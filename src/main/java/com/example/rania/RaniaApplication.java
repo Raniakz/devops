5f3ed4658baac66813b6af2e351aed02ad83b81f
Rania Gz <gz.rania216@gmail.com>
2025-12-05 19:47:05 +0100

    first

---
+++ src/main/java/com/example/rania/RaniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rania/RaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
